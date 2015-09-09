8a3b34237c0f23e3b0eb7e32a370c5a7c6c489e3
Marcus Krejpowicz <krebbl@gmail.com>
2015-09-09 17:08:37 +0200

    added rappid + cordova build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Building rappid app"
+rappidjs build
+echo "Builing cordova app"
+( cd cordova ; cordova build )
+echo "Starting emulator"
+ios-sim launch ./cordova/platforms/ios/build/emulator/Hiptees.app --devicetypeid "com.apple.CoreSimulator.SimDeviceType.iPhone-6"
+exit 1
+
+
+
+
