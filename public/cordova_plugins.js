24e966b0ddb0a59eba960a179fc421453009d38d
Marcus Krejpowicz <krebbl@gmail.com>
2015-10-01 11:24:54 +0200

    added cordova js libs

---
+++ public/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-statusbar": "1.0.1",
+    "com.phonegap.plugins.facebookconnect": "0.11.0"
+}
+// BOTTOM OF METADATA
+});
