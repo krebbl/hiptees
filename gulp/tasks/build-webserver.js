f84dd94c4b05468e36f9ba0e886465fb67167cc3
krebbl <krebbl@gmail.com>
2016-04-04 17:04:05 +0200

    updates ...

--- gulp/tasks/build-webserver.js
+++ gulp/tasks/build-webserver.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var proxy = require('http-proxy-middleware');
 
-gulp.task('webserver', function () {
-    gulp.src('public')
+gulp.task('build-webserver', function () {
+    gulp.src('public-build')
         .pipe(webserver({
             livereload: false,
             directoryListing: false,
@@ -16,7 +16,7 @@ gulp.task('webserver', function () {
             // 192.168.99.100:32773
             // http://api.hiptees-api.f0dc3c97.svc.dockerapp.io:32773
             middleware: [
-                proxy('/api/v1', {target: 'http://api.hiptees-api.f0dc3c97.svc.dockerapp.io:32773/', changeOrigin: false, xfwd: true}),
+                proxy('/api/v1', {target: 'http://192.168.99.100:32773/', changeOrigin: false, xfwd: true}),
                 proxy('/sprdApi/v1', {
                     target: 'http://api.spreadshirt.de', changeOrigin: true,
                     xfwd: true, pathRewrite: {"sprdApi": "api"}
